f3e0d0873fb47e07773fbd7820e65535c28a436f
Michael McConnell <msmcconnell@wpi.edu>
2017-02-02 13:49:31 -0500

    modified daq demo

--- daqDemo.m
+++ daqDemo.m
@@ -1,6 +1,16 @@
 mydaq = daq.createSession('ni');
 mydaq.addAnalogInputChannel('Dev5', 'ai1', 'Voltage');
-
-while 1
-    ai1_out = mydaq.inputSingleScan
+t = 0;
+tic;
+result = zeros(1000, 2);
+while t < 1000
+    ai1_out = mydaq.inputSingleScan;
+    t = t + 1;
+    result(t,:) = [toc, ai1_out];
 end
+
+figure;
+scatter(result(:,1), result(:,2), '.');
+xlabel('time (seconds)');
+ylabel('Voltage (V)');
+title('Free-load voltage collected from a FS-1000 LAT Probe')
