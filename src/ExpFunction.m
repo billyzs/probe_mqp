37dc4440c1857280829181fb635b7b2811cd1e8b
Michael McConnell <msmcconnell@wpi.edu>
2017-02-28 19:54:10 -0500

    Added exponential function class but forgot to git add the file

---
+++ src/ExpFunction.m
@@ -0,0 +1,46 @@
+classdef ExpFunction < handle
+    %EXPFUNCTION A class representing a first order exponential function
+    %   Detailed explanation goes here
+    
+    properties
+        a = 0;
+        b = 0;
+        y_0 = 0;
+    end
+    
+    methods
+        %Constructor
+        function this = ExpFunction(a, b, y_0)
+            this.a = a;
+            this.b = b;
+            this.y_0 = y_0;
+        end
+        
+        function y = getY(this, x)
+            y = this.a*exp(this.b*x) + this.y_0;
+        end
+        
+        function [ A, B, y_0] = estimateExp(this, y, x, y_0, updateSelf)
+            if nargin < 5
+                updateSelf = false;
+            end
+            %assume y = A*exp(Bx) + y_0, estimate A, B
+
+            y_hat = log(y - y_0*0.999);
+
+            % y_hat = log(A) +  B * x
+            p = polyfit(x, y_hat, 1);
+
+            B = p(1);
+            A = exp(p(2));
+            if updateSelf
+                this.a = A;
+                this.b = B;
+                this.y_0 = y_0;
+            end
+
+        end
+    end
+    
+end
+
